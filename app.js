06fce5b3ad2b72738eb8eaa250c45065ee6de9ad
Silje32 <msh070425@gmail.com>
2025-06-30 10:59:42 +0200

    Created my elements

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const listContainer = document.querySelector("#list-container");
+
 let tasks = [];
 
 const taskForm = document.querySelector("#task-form");
@@ -16,4 +18,27 @@ taskForm.addEventListener("submit", (e) => {
     description: userInput,
     completed: false,
   });
+  renderPage();
 });
+
+const builldPage = (tasksArr) => {
+  listContainer.replaceChildren();
+  tasksArr.forEach((task) => {
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+
+    const timestampElement = document.createElement("p");
+    timestampElement.classList.add("datetime");
+    timestampElement.textContent = task.timestamp;
+
+    const descriptionElement = document.createElement("p");
+    descriptionElement.classList.add("description");
+    descriptionElement.textContent = task.description;
+
+    taskContainer.append(timestampElement, descriptionElement);
+    listContainer.prepend(taskContainer);
+  });
+};
+const renderPage = () => {
+  builldPage(tasks);
+};
